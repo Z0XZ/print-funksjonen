d7603fcf289867449e7dac09ce45f6a47b2e5cb1
Jon Bjarne Bø <77270569+Z0XZ@users.noreply.github.com>
2023-08-17 00:41:26 +0200

    Create oppgave2.py

---
+++ oppgave2.py
@@ -0,0 +1,10 @@
+# Linjene under som starter med # skal du undersøke litt nærmere. Forsøk å se hva som skjer og rett opp eventuelle feil slik at det ikke kommer noen error og det ikke er noen regnefeil.
+
+print("Hello", "World", "!") #Hva skjer her?
+#print(Hello World!)
+#print(55 * 3)
+#print("12"*3)
+#print(4*"8") #Regner ut 4 * 8 = 32
+
+
+# Klarer du å få programmet til å printe "heisann! " 100 ganger uten å skrive 100 print-funksjoner?
